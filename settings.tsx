4b602bc4aaa42aa0ca890d85a0d43821f939d6ca
papichulooo69 <pleifer.paul@gmx.de>
2025-03-24 18:10:27 +0100

    .

--- settings.tsx
+++ settings.tsx
@@ -131,7 +131,7 @@ export const COMMANDS: Command[] = [
     },
     {
         name: "/join-channel",
-        description: "Trete privaten Textkanälen bei.",
+        description: "Trete einem privaten Textkanälen bei.",
         categories: ["Mitglieder"],
         parameters: [{
             name: "password",
