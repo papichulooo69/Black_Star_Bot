c2523ae20c6a471f5d0cbcc639dcf7aad03ab038
papichulooo69 <pleifer.paul@gmx.de>
2025-05-10 13:42:09 +0200

    update

--- components/header.tsx
+++ components/header.tsx
@@ -4,7 +4,7 @@ import { DISCORD_LINK } from "@/settings";
 import React from "react";
 
 const Header = () => {
-    return <header className="bg-gray-900/50 backdrop-blur-sm border-b border-gray-800">
+    return <header className="sticky top-0 z-50 bg-gray-900/50 backdrop-blur-md border-b border-gray-800">
         <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className="flex items-center justify-between h-16">
                 <div className="flex items-center">
